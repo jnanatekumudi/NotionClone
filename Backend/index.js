06a1d3a19d2e0986fb77a49e3231680669943833
jnanatekumudi <t.jnana.444@gmail.com>
2021-01-07 20:15:10 +0530

    add .gitignore and change EOL settings

--- Backend/index.js
+++
@@ -1,9 +0,0 @@
-const { response } = require('express');
-const express = require('express');
-
-const app = express();
-
-app.get('/health', function (req, res, next) {
-
-    res.json({ status: 'UP' });
-});
