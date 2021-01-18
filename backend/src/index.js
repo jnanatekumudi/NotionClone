a43ba778e585f5b0862b27afcb011d917908cca2
Tan-2029 <staneshqa@gmail.com>
2021-01-18 09:03:35 +0300

    Bootstrap backend server

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,14 +2,12 @@ const { Router } = require('express');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT||4000;
-  
-  app.listen(PORT, () => {
-    console.log(`Running at http://localhost:${PORT}`)
-  })
-
 
 app.get('./health',function(req,res,next){
     res.json({status: 'UP'}); 
 }
 )
-app.use("/health", Router);
+
+app.listen(PORT, () => {
+  console.log(`Running at http://localhost:${PORT}`)
+})
